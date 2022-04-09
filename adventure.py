ff81bd39908c433efc6a297b6cfdf3db8259cece
Daniel Harris <dan_harris10@yahoo.com>
2022-04-08 20:21:09 -0600

    added player, monster scripts, drop functionality, player types, health display

--- adventure.py
+++ adventure.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 from rooms import rooms
+from player import player
 import sys
+from os import system, name
 
 # Replace RPG starter project with this code when new instructions are live
 
@@ -16,24 +18,53 @@ Commands:
 ''')
 
 
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
 def showStatus():
+    clear()
     # print the player's current status
     print('---------------------------')
     print('You are in the ' + currentRoom)
+    print()
     # print the current inventory
     print('Inventory : ' + str(inventory))
+    print('Health: ', player_type["health"])
+    print()
+    print(rooms[currentRoom]['description'])
+    print()
+
     # print an item if there is one
     if "item" in rooms[currentRoom]:
         print(f"You see a {rooms[currentRoom]['item']}")
-    print("---------------------------")
+        print("---------------------------")
 
 
+player_type = ''
+
+
+def choose_player():
+    global player_type
+    player_info = ''
+    while player_info == '':
+        player_info = input("What is your game play style? ([A]ggressive or [D]efensive): ").lower()
+
+        if player_info == "A" or player == "aggressive":
+            player_type = player["Attack"]
+        else:
+            player_type = player["Defensive"]
+
+
+choose_player()
 # an inventory, which is initially empty
-inventory = []
-
-# a dictionary linking a room to other rooms
-## A dictionary linking a room to other rooms
-
+inventory = player_type['inventory']
 
 # start the player in the Hall
 currentRoom = 'Basement'
@@ -73,10 +104,14 @@ while True:
         if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
             # add the item to their inventory
             inventory.append(move[1])
+
             # display a helpful message
             print(move[1] + ' got!')
             # delete the item from the room
-            del rooms[currentRoom]['item']
+            item_index = rooms[currentRoom]['item'].index(move[1])
+            del rooms[currentRoom]['item'][item_index]
+            if len(rooms[currentRoom]['item']) == 0:
+                del rooms[currentRoom]['item']
         # otherwise, if the item isn't there to get
         else:
             # tell them they can't get it
@@ -87,6 +122,22 @@ while True:
             print(f"{move[1]} equipped!")
         else:
             print(f"You don't have a {move[1]} in your inventory!")
+
+    if move[0] == 'drop':
+        # check that they are allowed wherever they want to go
+        if move[1] in inventory:
+            # set the current room to the new room
+            if 'item' not in rooms[currentRoom]:
+                rooms[currentRoom]['item'] = [move[1]]
+                inventory.remove(move[1])
+                print(f'{move[1]} was dropped')
+            else:
+                rooms[currentRoom]['item'].append(move[1])
+                inventory.remove(move[1])
+                print(f'{move[1]} was dropped')
+        # there is no door (link) to the new room
+        else:
+            print("You currently have nothing to drop")
 
     if move[0] == 'q' or move[0] =='quit':
         quitResponse = input('Are you you sure you want to quit? (Y|N): ').strip().lower()
---
+++ monster.py
@@ -0,0 +1,16 @@
+monsters = {
+    "Level-1": [
+        {
+            'attack': [5, 10, 15, 20],
+            'type': 'mini-boss',
+            'health': 150
+        },
+        {
+            'attack': [10, 20, 30, 40],
+            'type': 'boss',
+            'health': 300
+        }
+    ]
+
+}
+
