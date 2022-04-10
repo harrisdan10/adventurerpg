8dd849b1ebb16f16ac8b69038bf5054899e9786e
Daniel Harris <dan_harris10@yahoo.com>
2022-04-09 22:27:27 -0600

    added player, monster scripts, drop functionality, player types, health display

---
+++ dictionaries/items.py
@@ -0,0 +1,15 @@
+items = {
+           'Basement':
+                [{
+                    'note': {
+                        'description': 'placeholder note'
+                    },
+                    'oil': {
+                        'description': 'placeholder oil'
+                    },
+                    'torch': {
+                        'description': 'placeholder torch'
+                    }
+                }]
+
+}
